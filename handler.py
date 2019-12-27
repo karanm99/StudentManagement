d7dd3eaf45287ba8e0ebaff0b2cf503d27282e6f
Karan Meghani <karanm99@gmail.com>
2019-12-27 14:40:48 +0530

    Pushed application for CRUD using Flask

---
+++ handler.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+"""
+handler.py has different Class which deals with APIs of Student and StudentClass
+"""
+
+import logging
+from flask import request, render_template
+from models import DB as db, APP as app
+from models import StudentDAO
+from models import StudentClassDAO
+
+db.init_app(app)
+
+@app.route('/')
+def show_all():
+    """
+    Fetches all the students and class from db using DAO
+    and render it on UI
+    :return:
+    """
+    try:
+        all_students = StudentDAO.get_all_students()
+        all_classes = StudentClassDAO.get_all_classes()
+        logging.info("Successfully fetched data")
+    except Exception as exception:  # pylint: disable=broad-except
+        logging.warning("Failed to load data due to %s", exception)
+    return render_template('HomePage.html', students=all_students, std_class=all_classes)
+
+@app.route('/add_student', methods=['POST'])
+def add_student():
+    """
+    POST method use to add student in the db using StudentDAO
+    :return: response message along with status code
+    """
+    if request.method == 'POST':
+        try:
+            class_id = request.form.get('class_id')
+            student_name = request.form.get('student_name')
+            StudentDAO.add_student(class_id=class_id, student_name=student_name)
+            logging.info("Student was successfully added")
+            return ({'message':'Student successfully created '}, 200)
+        except Exception as exception:  # pylint: disable=broad-except
+            logging.error("Failed to add students due to %s", exception)
+            return ({'message':'Failed to add student'}, 400)
+    return ({'message': 'Bad request'}, 404)
+
+@app.route('/delete_student', methods=['POST'])
+def delete_student():
+    """
+    POST method to delete a existing student from db
+    :return: response message along with status code
+    """
+    if request.method == 'POST':
+        try:
+            student_id = request.form.get('student_id')
+            course_id = request.form.get('course_id')
+            StudentDAO.delete_student(id=student_id, class_id=course_id)
+            logging.info("Student deleted successfully")
+            return ({'message':'Successfully deleted'}, 200)
+        except Exception as exception:  # pylint: disable=broad-except
+            logging.error("Failed to delete student because %s", exception)
+            return {'message':'Failed to delete student'}, 400
+    return ({'message': 'Bad request'}, 404)
+
+@app.route('/update_student', methods=['PUT'])
+def update_student():
+    """
+    PUT method to update existing student from db
+    :return:response message along with status code
+    """
+    if request.method == 'PUT':
+        try:
+            student_id = request.form.get('student_id')
+            course_id = request.form.get('course_id')
+            student_name = request.form.get('student_name')
+            StudentDAO.update_student(student_id, student_name)
+            logging.info("Successfully updated the student details of course %s", course_id)
+            return ({'message':'Student updated successfully'}, 200)
+        except Exception as exception:  # pylint: disable=broad-except
+            logging.error("Failed to updated the student details due to %s", exception)
+            return ({'message':'Failed to update student'}, 400)
+    return ({'message': 'Bad request'}, 404)
+
+@app.route('/assign_students', methods=['POST'])
+def assign_class_to_students():
+    """
+    POST method to assign student to the class
+    :return: response message along with status code
+    """
+    if request.method == 'POST':
+        req_dict = request.form.to_dict()
+        response = {}
+        for student_details in req_dict:
+            student_id = student_details
+            try:
+                course_id = req_dict[student_details]
+                StudentDAO.assign_class(class_id=course_id, student_id=student_id)
+                logging.info("Class assigned to %s successfully", student_id)
+                response[student_id] = "Class assigned successfully"
+            except Exception as excpetion:  # pylint: disable=broad-except
+                response[student_id] = "Failed to assign class"
+                logging.error("Failed to assign class to %s due to %s", student_id, excpetion)
+        return ({'message':response}, 200)
+
+    return ({'message': 'Bad request'}, 404)
+@app.route('/add_class', methods=['POST'])
+def add_class():
+    """
+    POST method to add a class in DB using StudentClassDAO
+    :return: response message along with status code
+    """
+    if request.method == 'POST':
+        try:
+            class_name = request.form.get('class_name')
+            StudentClassDAO.add_class(class_name)
+            logging.warning("Successfully class added")
+            return ({'message':'Class added successfully'}, 200)
+        except Exception as exception:  # pylint: disable=broad-except
+            logging.warning("Failed to add class due to %s", exception)
+            return ({'message':'Failed to add class'}, 400)
+    return ({'message': 'Bad request'}, 404)
+
+@app.route('/update_class_details', methods=['PUT'])
+def update_class_details():
+    """
+    PUT method to update the existing class functionality
+
+    :return: response message along with status code
+    """
+    if request.method == 'PUT':
+        try:
+            class_id = request.form.get('class_id')
+            class_name = request.form.get('class_name')
+            class_leader_id = request.form.get('class_leader_id')
+            StudentClassDAO.update_class_details(class_id=class_id, class_name=class_name,
+                                                 class_leader_id=class_leader_id)
+            logging.info("Successfully updated the class details")
+            return ({'message':'Class details updated successfully '}, 200)
+        except Exception as exception:  # pylint: disable=broad-except
+            logging.error("Failed to update class details due to %s", exception)
+            return ({'message':'Failed to updated class details'}, 400)
+
+    return ({'message': 'Bad request'}, 404)
+@app.route('/showCourse/<course_id>', methods=['GET'])
+def display_course(course_id):
+    """
+    Get method to get data related to particular class
+    and render it on UI for course
+
+    :param course_id:
+    :return:
+    """
+    if request.method == 'GET':
+        try:
+            students = StudentDAO.get_students_class_id(class_id=course_id)
+            unassigned_students = StudentDAO.get_all_unassigned_students()
+            selected_class = StudentClassDAO.get_class_by_id(course_id)
+            leader = StudentDAO.get_student_by_id(selected_class.class_leader)
+            logging.info("Successfully fetched data from db")
+        except Exception as exception:  # pylint: disable=broad-except
+            logging.warning("Failed to fetch due to %s", exception)
+    return render_template('course.html', students=students,
+                           std_class=selected_class, un_student=unassigned_students, leader=leader)
+
+if __name__ == '__main__':
+    db.create_all()
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,340 @@
+#!/usr/bin/env python
+"""
+models.py has different Class which deals with DAO of Student and StudentClass
+"""
+
+import datetime
+from uuid import uuid4 as gen_uuid
+from typing import NewType
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.ext.declarative import declarative_base
+
+# new type
+Uuid = NewType('Uuid', str)
+
+APP = Flask(__name__)
+APP.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:root@localhost:5432/studentManagement'
+APP.config['SECRET_KEY'] = "random string"
+APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+DB = SQLAlchemy(APP)
+Base = declarative_base()
+
+
+def uuid4():
+    """
+    Method to generate uuid as primary key for student and student_class
+    :return: string as uuid
+    """
+    return str(gen_uuid())
+
+
+class Student(DB.Model, Base):
+    """
+        This is a class for db connection with PostgreSQL.
+        Attributes:
+            id (int): ID of student.
+            name (string): name of student.
+            class_id (int): class id of student.
+            created_on (datetime): created on time and date.
+            updated_on (datetime): updated on time and date.
+    """
+
+    # id = DB.Column(DB.Integer, primary_key=True, autoincrement=True)
+    id = DB.Column(UUID, primary_key=True, default=uuid4)
+    name = DB.Column(DB.String(100))
+    class_id = DB.Column(UUID, nullable=True, index=True)
+    created_on = DB.Column(DB.DateTime)
+    updated_on = DB.Column(DB.DateTime, index=False, nullable=True)
+
+    def __init__(self, name, class_id=None, updated_on=None):
+        """
+        __init__ method to initialize the Student Class
+        :param name:
+        :param class_id:
+        :param updated_on:
+        """
+        self.name = name
+        self.class_id = class_id
+        self.updated_on = updated_on
+        self.created_on = datetime.datetime.now()
+
+    @property
+    def get_class_id(self):
+        """
+        :return: class id of object
+        """
+        return self.class_id
+
+    @property
+    def get_name(self):
+        """
+        :return: returns the name of the class
+        """
+        return self.name
+
+    @property
+    def get_created_on(self):
+        """
+        :return: returns the time of creation
+        """
+        return self.created_on
+
+
+class StudentDAO(Student):
+    """
+        This is a Student data access object of class Student
+        for db connection with PostgreSQL. Performs CRUD Operations
+    """
+
+    @staticmethod
+    def add_student(class_id=None, student_name=None):
+        """
+        Method to add student with student_name
+
+        :param class_id:
+        :param student_name:
+        :return: student object
+        """
+        student = None
+        if student_name and class_id:
+            student = Student(name=student_name, class_id=class_id)
+            DB.session.add(student)
+            DB.session.commit()
+        elif student_name:
+            student = Student(name=student_name)
+            DB.session.add(student)
+            DB.session.commit()
+        return student
+
+
+    @staticmethod
+    def get_all_students():
+        """
+        get all student method to get all the students rows from Student table
+        :return: all student rows from student table
+        """
+        students = Student.query.all()
+        return students
+
+    @staticmethod
+    def get_student_by_id(student_id):
+        """
+        :param student_id:
+        :return: the student object with given student_id
+        """
+        student = None
+        if student_id:
+            student = Student.query.filter_by(id=student_id).first()
+            if student:
+                return student
+        return student
+
+    @staticmethod
+    def get_students_class_id(class_id):
+        """
+        :param class_id:
+        :return: all the students have the with class_id as class_id
+        """
+        students = []
+        if class_id:
+            students = Student.query.filter_by(class_id=class_id).all()
+            if students:
+                return students
+        return students
+
+    @staticmethod
+    def get_student_by_multiple_filter(**kwargs):
+        """
+        get student by multiple filter
+
+        :param kwargs:
+        :return:
+        """
+        students = None
+        if kwargs:
+            students = Student.query.filter_by(**kwargs).first()
+            if students:
+                return students
+        return students
+
+    @staticmethod
+    def get_all_unassigned_students():
+        """
+        get_all_unassigned_students
+
+        :return:
+        """
+        student_with_none = []
+        student_with_none = Student.query.filter_by(class_id=None).all()
+        return student_with_none
+
+    @staticmethod
+    def assign_class(class_id=None, student_id=None):
+        """
+        assign class to students
+        :param class_id:
+        :param student_id:
+        :return:
+        """
+        if class_id and student_id:
+            student = StudentDAO.get_student_by_id(student_id)
+            student.class_id = class_id
+            DB.session.add(student)
+            DB.session.commit()
+
+    @staticmethod
+    def update_student(student_id, student_name=None):
+        """
+        update student with student name
+
+        :param student_id:
+        :param student_name:
+        :return:
+        """
+        student_obj = StudentDAO.get_student_by_id(student_id)
+        if student_obj:
+            student_obj.name = student_name
+            student_obj.updated_on = datetime.datetime.now()
+            DB.session.add(student_obj)
+            DB.session.commit()
+
+    @staticmethod
+    def delete_student(**kwargs):
+        """
+        delete existing student by searching student with
+        multiple filter e.g. student_id, class_id
+        :param kwargs:
+        :return:
+        """
+        student = StudentDAO.get_student_by_multiple_filter(**kwargs)
+        if student:
+            classes = StudentClassDAO.get_all_class_by_leader(student.id)
+            if classes:
+                classes.class_leader = None
+                DB.session.add(classes)
+                DB.session.commit()
+
+            DB.session.delete(student)
+            DB.session.commit()
+
+
+class StudentClass(DB.Model, Base):
+    """
+    This is a StudentClass for db connection with PostgreSQL.
+    Attributes:
+        id (int): ID of class.
+        name (string): name of class.
+        class_id (int): student id of class leader.
+        created_on (datetime): created on time and date.
+        updated_on (datetime): updated on time and date.
+    """
+
+    id = DB.Column(UUID, primary_key=True, index=True, default=uuid4)
+    name = DB.Column(DB.String(100))
+    class_leader = DB.Column(UUID, nullable=True, index=True)
+    created_on = DB.Column(DB.DateTime)
+    updated_on = DB.Column(DB.DateTime, index=False, nullable=True)
+
+    def __init__(self, name):
+        """
+        initialization of student class object
+
+        :param name:
+        """
+        self.name = name
+        self.created_on = datetime.datetime.now()
+
+    @property
+    def get_name(self):
+        """
+        :return: name of the class
+        """
+        return self.name
+
+    @property
+    def get_class_leader(self):
+        """
+        :return: class_leader for particular class
+        """
+        return self.class_leader
+
+    @property
+    def get_created_on(self):
+        """
+        :return: time of creation
+        """
+        return self.created_on
+
+
+class StudentClassDAO(StudentClass):
+    """
+        This is a StudentClass Data access object for db connection with
+        PostgreSQL and perform CRUD operations
+    """
+
+    @staticmethod
+    def add_class(class_name):
+        """
+        To create a class with given class_name
+        :param class_name:
+        :return:
+        """
+        if class_name:
+            classes = StudentClass(name=class_name)
+            DB.session.add(classes)
+            DB.session.commit()
+
+    @staticmethod
+    def get_all_classes():
+        """
+        To get all the classes from db
+        :return: all classes objects
+        """
+        classes = StudentClass.query.all()
+        return classes
+
+    @staticmethod
+    def get_class_by_id(class_id):
+        """
+        To get class object by id
+        :param class_id:
+        :return: class object
+        """
+        current_class = None
+        if class_id:
+            current_class = StudentClass.query.filter_by(id=class_id).first()
+            if current_class:
+                return current_class
+        return current_class
+
+    @staticmethod
+    def update_class_details(class_id=None, class_name=None, class_leader_id=None):
+        """
+        To update the existing class details by provind various fields
+        :param class_id:
+        :param class_name:
+        :param class_leader_id:
+        :return:
+        """
+        class_obj = StudentClassDAO.get_class_by_id(class_id)
+        if class_obj and (class_leader_id or class_name):
+            if class_leader_id:
+                class_obj.class_leader = class_leader_id
+            if class_name:
+                class_obj.name = class_name
+            class_obj.updated_on = datetime.datetime.now()
+            DB.session.add(class_obj)
+            DB.session.commit()
+
+    @staticmethod
+    def get_all_class_by_leader(student_id):
+        """
+        To get all the classes by leader
+        :param student_id:
+        :return: class object
+        """
+        classes = StudentClass.query.filter_by(class_leader=student_id).first()
+        return classes
